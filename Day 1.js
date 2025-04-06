ac55e9d8dfc1bacd1e42fe4c063628064b123db4
Amrit1512 <amritraj15122000@gmail.com>
2025-04-06 12:02:39 +0530

    Day 1

---
+++ Day 1.js
@@ -0,0 +1,53 @@
+/* The first day of this challenge is know more about "Variable & Datatypes"
+-------Activity:1(Variable declarations)---------------*/
+
+var number = 22; //task-1
+console.log(number);
+
+let string = "amrit raj" // task-2
+console.log(string);
+
+/* ------------------Activity:-2 (Constant declarations)--------------*/
+
+
+const bool = "true"; //task-3
+console.log(bool); 
+
+/* -----------Activity:-3(Data types)----------------*/
+
+console.log("datatype of number is:", typeof number); //task-4
+console.log("datatype of string is:", typeof string);
+console.log("datatype of boolean is:", typeof bool); 
+
+let obj = {
+    name : "amrit raj",
+    age : 22
+}
+console.log("datatype of object is:", typeof obj)
+
+var arr = [1,2,3,4,5];
+console.log("datatype of array is:", typeof arr)
+
+/* -----------Activity:-4(Reassigning variables)----------------*/
+
+// as i already declared variable using let in "task-2" so now i just reuse it 
+string = "new value of string";  //task-5
+console.log(string);
+
+/* -----------Activity:-5(Understanding const)----------------*/
+
+// as i already declared variable using const in "task-3" so now i just reuse it
+//task-6
+bool = "false";  
+console.log(bool)
+
+
+
+
+
+
+
+
+
+
+
